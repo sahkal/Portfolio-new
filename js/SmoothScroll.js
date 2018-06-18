f9bae3863b8cf727e8fc192808fbdf80382dedcc
Sahkal Poddar <sahkalplanet@gmail.com>
2018-06-18 14:48:38 +0530

    Add files via upload

---
+++ js/SmoothScroll.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	var scrollLink= $('.scroll');
+	//Smooth scrolling
+	scrollLink.click(function(e){
+		e.preventDefault();
+		$('body,html').animate({
+			scrollTop:$(this.hash).offset().top
+		},1300);
+	})
+})
